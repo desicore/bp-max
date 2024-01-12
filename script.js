5858b5c04601d255e21c3b0e6aee2a702ed4ee93
desicore <67513424+desicore@users.noreply.github.com>
2024-01-12 18:49:35 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+// Click on nav logo scrolls to top of the page
+$(".navbar4_logo").click(function () {
+  $("html, body").animate({ scrollTop: "0", easing: "easeout" }, 1000);
+});
+
+// Features section scroll animation
+
+// FEATURES GSAP SCROLL ANIMATION
+let trigger = ".content_trigger";
+let visual = ".visual_item";
+let section = ".section_sticky";
+
+// On page load
+$(section).each(function (index) {
+  $(this).find(trigger).first().addClass("is-active");
+  $(this).find(visual).first().addClass("is-active");
+});
+
+// On scroll into view
+$(trigger).each(function (index) {
+  let myIndex = $(this).index();
+  let mySection = $(this).closest(section);
+  let targetElement = mySection.find(visual).eq(myIndex);
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: $(this),
+      // trigger element - viewport
+      start: "top center",
+      end: "bottom center",
+      onEnter: () => {
+        mySection.find(".is-active").removeClass("is-active");
+        $(this).addClass("is-active");
+        targetElement.addClass("is-active");
+      },
+      onEnterBack: () => {
+        mySection.find(".is-active").removeClass("is-active");
+        $(this).addClass("is-active");
+        targetElement.addClass("is-active");
+      }
+    }
+  });
+});
